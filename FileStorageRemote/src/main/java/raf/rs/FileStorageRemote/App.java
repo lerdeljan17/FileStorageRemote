35e7e10db707ab07b93687c6305e08aa97fddbd4
lerdeljan17 <lerdeljan17@raf.rs>
2019-11-05 13:07:21 +0100

    ffs

---
+++ FileStorageRemote/src/main/java/raf/rs/FileStorageRemote/App.java
@@ -0,0 +1,13 @@
+package raf.rs.FileStorageRemote;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
